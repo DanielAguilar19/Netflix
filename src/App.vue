c9996cc85cd26b606c5e530807afa0b6bf94cd86
Daniel Aguilar <aguilardaniel631@gmail.com>
2024-07-11 21:17:34 -0600

    Creacion de homeview para el proyecto de netflix

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <homePage></homePage>
+</template>
+
+<script>
+import homePage from "./components/homePage.vue";
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components:{
+    homePage,
+  }
+});
+</script>
