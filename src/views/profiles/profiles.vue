d2fdad1427882255c6c8e3922691a53d0fa4bf84
Daniel Aguilar <aguilardaniel631@gmail.com>
2024-08-09 13:58:18 -0600

    finalizacion de view de perfiles

--- src/views/profiles/profiles.vue
+++ src/views/profiles/profiles.vue
@@ -1,24 +1,28 @@
 <template>
     <div class="w-full h-screen bg-black flex flex-col items-center justify-center text-white">
-      <h1 class="text-3xl mb-8">Who's watching?</h1>
+      <h1 class="text-3xl mb-15">Who's watching?</h1>
       <div class="flex space-x-8">
-        <div v-for="usuario in usuarios" :key="usuario.nombre" class="flex flex-col items-center">
-          <img :src="(`${usuario.perfil}`)" alt="Profile Image" class="w-32 h-32 rounded-lg mb-2"/>
+        <router-link  to="/home">
+          <button v-for="usuario in usuarios" :key="usuario.nombre">
+          <img :src="getProfileImage(usuario.perfil)" alt="Profile Image" class="flex align-middle w-32 h-32 rounded-lg mb-2 mr-5"/>
           <label>{{ usuario.nombre }}</label>
-        </div>
+        </button>
+        </router-link>
         <div class="flex flex-col items-center">
-          <div class="w-32 h-32 flex items-center justify-center bg-gray-600 rounded-lg mb-2">
+          <router-link to="/home">
+            <button class="w-32 h-32 flex items-center justify-center bg-gray-600 rounded-lg mb-2">
             <svg class="w-12 h-12 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path></svg>
-          </div>
+          </button>
+          </router-link>
           <label>Add Profile</label>
         </div>
       </div>
     </div>
   </template>
 
+
 <script setup>
 import { ref } from 'vue';
-
 const usuarios = ref([
   {
     nombre: 'Daniel',
@@ -31,11 +35,8 @@ const usuarios = ref([
     perfil: 'Avatar_2.png'
   }
 ]);
+
+const getProfileImage = (imageName) => {
+  return new URL(`/src/assets/${imageName}`, import.meta.url).href;
+};
 </script>
-
-
-<style scoped>
-img{
-    src: "../../assets/Avatar_1.png";
-}
-</style>
